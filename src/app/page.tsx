303ea2dec391a5659a4e5f658c1238e8e3c06bcf
yara-hue <yaraehabnoureldin@gmail.com>
2026-06-23 07:24:50 +0300

    feat: remove FeaturedDivisions section, expand FeaturedProducts to masonry grid with all products and hover overlays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/sections/home/Hero";
 import { CompanyOverview } from "@/components/sections/home/CompanyOverview";
 import { Capabilities } from "@/components/sections/home/Capabilities";
-import { FeaturedDivisions } from "@/components/sections/home/FeaturedDivisions";
 import { FeaturedProducts } from "@/components/sections/home/FeaturedProducts";
 import { Approach } from "@/components/sections/home/Approach";
 import { FutureVision } from "@/components/sections/home/FutureVision";
@@ -13,7 +12,6 @@ export default function Home() {
       <Hero />
       <CompanyOverview />
       <Capabilities />
-      <FeaturedDivisions />
       <FeaturedProducts />
       <Approach />
       <FutureVision />
